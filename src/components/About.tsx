fcda6d48516fcf89e9265a525317b7ed40f4022c
nikolabulatovic <bulatovic_nikola@yahoo.com>
2025-06-08 15:51:43 +0200

    feat: update 'About' section by adding image and moving text to the right

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import Image from 'next/image';
 import { motion } from 'framer-motion';
 import { useTranslations } from 'next-intl';
 import SectionHeading from './SectionHeading';
@@ -8,7 +9,23 @@ const About = () => {
   const t = useTranslations('about');
 
   return (
-    <section id='about' className='min-h-screen bg-primary py-20'>
+    <section
+      id='about'
+      className='flex flex-col md:flex-row items-center gap-20 bg-primary py-20 px-10'>
+      <motion.div
+        initial={{ opacity: 0, x: -50 }}
+        whileInView={{ opacity: 1, x: 0 }}
+        viewport={{ once: true }}
+        transition={{ duration: 1.2, delay: 0.8, ease: 'easeOut' }}
+        className='w-full aspect-[7/3]'>
+        <Image
+          src='/images/covek-o-nama.jpg'
+          alt='O nama'
+          width={1000}
+          height={1000}
+          className='object-cover'
+        />
+      </motion.div>
       <div className='container mx-auto px-4'>
         <div className='flex flex-col lg:flex-row items-center gap-28'>
           <motion.div
@@ -16,7 +33,7 @@ const About = () => {
             whileInView={{ opacity: 1, x: 0 }}
             viewport={{ once: true }}
             transition={{ duration: 0.8, ease: 'easeOut' }}
-            className='space-y-6 w-1/2'>
+            className='space-y-6'>
             <SectionHeading title={t('title')} />
             <motion.div
               className='font-poppins space-y-4 text-secondary text-xl text-shadow-[0_3px_5px_rgb(0_0_0_/_0.15)]'
