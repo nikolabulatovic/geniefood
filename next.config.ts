8d2dcb61b8df8fbebead251bccc3b6c718f6154f
nikolabulatovic <bulatovic_nikola@yahoo.com>
2025-10-16 22:18:37 +0200

    chore: update code for deploy

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,14 @@
 import { NextConfig } from 'next';
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const config: NextConfig = {
   /* config options here */
+  output: 'export',
+  basePath: isProd ? '/geniefood' : '',
+  assetPrefix: isProd ? '/geniefood/' : '',
   images: {
+    unoptimized: true,
     domains: ['geniefood.rs'],
   },
 };
