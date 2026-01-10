45c6e9b13df6c0b2f89ac8b518f1a09d2319a97b
nikolabulatovic <bulatovic_nikola@yahoo.com>
2026-01-10 13:00:46 +0100

    feat(Metadata): add og and twitter card meta properties

--- src/app/en/layout.tsx
+++ src/app/en/layout.tsx
@@ -1,10 +1,23 @@
 import type { Metadata } from 'next';
 import '../globals.css';
 
+const description =
+  'Gen!e is a brand by Macrobiotic Prom, a company making plant-based food for 37 years. Our goal is simple: create meals that are healthier, taste amazing, and respect the planet.';
+
 export const metadata: Metadata = {
   title: 'Genie Food',
-  description:
-    'Gen!e is a brand by Macrobiotic Prom, a company making plant-based food for 37 years. Our goal is simple: create meals that are healthier, taste amazing, and respect the planet.',
+  description,
+  openGraph: {
+    title: 'Genie Food',
+    description,
+    images: ['https://geniefood.rs/images/genie-cover.jpg'],
+    url: 'https://geniefood.rs/en',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    images: ['https://geniefood.rs/images/genie-cover.jpg'],
+  },
 };
 
 export default function RootLayout({
--- src/app/sr/layout.tsx
+++ src/app/sr/layout.tsx
@@ -1,10 +1,23 @@
 import type { Metadata } from 'next';
 import '../globals.css';
 
+const description =
+  'Gen!e je brend kompanije Macrobiotic Prom, koja već više od 37 godina pravi zdraviju hranu na biljnoj bazi. Naša misija je jednostavna: učiniti biljne obroke neverovatno ukusnim, zdravijim, jednostavnim za korišćenje i dobrim za planetu.';
+
 export const metadata: Metadata = {
   title: 'Genie Food',
-  description:
-    'Gen!e je brend kompanije Macrobiotic Prom, koja već više od 37 godina pravi zdraviju hranu na biljnoj bazi. Naša misija je jednostavna: učiniti biljne obroke neverovatno ukusnim, zdravijim, jednostavnim za korišćenje i dobrim za planetu.',
+  description,
+  openGraph: {
+    title: 'Genie Food',
+    description,
+    images: ['https://geniefood.rs/images/genie-cover.jpg'],
+    url: 'https://geniefood.rs/sr',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    images: ['https://geniefood.rs/images/genie-cover.jpg'],
+  },
 };
 
 export default function RootLayout({
